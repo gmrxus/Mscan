794f1867d052d6d52f6cbab35cb362b4b1195c89
gmrxus <gmrxus@126.com>
2016-07-22 11:28:32 +0800

    创建

---
+++ app/src/main/java/cn/mrxus/mscan/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.mrxus.mscan.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.mrxus.mscan.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
